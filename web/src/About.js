22aaa49ad13e669ca6aaa2d2886eaca4fbabab57
Conke Hu <conke.hu@gmail.com>
2017-10-31 18:51:39 +0800

    add axios
    Signed-off-by: Conke Hu <conke.hu@gmail.com>

--- web/src/About.js
+++ web/src/About.js
@@ -3,7 +3,8 @@ import { Link } from 'react-router-dom';
 
 const About = () => (
     <div>
-        <h1>About WitDev</h1>
+        <h3>About WitDev</h3>
+        <h1>梧桐雨三架马车</h1>
         <b><Link to='/'>back to home</Link></b>
     </div>
 );
--- web/src/Home.js
+++ web/src/Home.js
@@ -1,10 +1,32 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Get } from 'react-axios';
 
 const Home = () => (
     <div>
         <h1>Home</h1>
-        <b><Link to='/about'>about us</Link></b>
+        <div>
+            <Get url="/api/customers/2">
+            {(error, response, isLoading) => {
+                if(error) {
+                    return (<div>Something bad happened: {error.message}</div>)
+                }
+                else if(isLoading) {
+                    return (<div>Loading...</div>)
+                }
+                else if(response !== null) {
+                    const customer = response.data;
+                    return (
+                        <div>
+                        Name: {customer.firstName} <br/>
+                        Age: {customer.age}
+                        </div>)
+                }
+                return (<div>Default message before request is made.</div>)
+            }}
+            </Get>
+        </div>
+        <h3><Link to='/about'>About us</Link></h3>
     </div>
 );
 
