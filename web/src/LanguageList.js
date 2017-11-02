b2a172a0c954ce43b4947e368f0ea52a63e92971
Sandy <rongzhou84@gmail.com>
2017-11-02 18:21:46 +0800

    add material ui
    Signed-off-by: Sandy <rongzhou84@gmail.com>

--- web/src/LanguageList.js
+++ web/src/LanguageList.js
@@ -1,11 +1,30 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { withAxios } from 'react-axios';
+import { withStyles } from 'material-ui/styles';
+import List, { ListItem } from 'material-ui/List';
+import Grid from 'material-ui/Grid';
+import Checkbox from 'material-ui/Checkbox';
+import PropTypes from 'prop-types';
+
+
+const styles = theme => ({
+    root: {
+        flexGrow: 1,
+    },
+    list: {
+        width: '100%',
+        background: theme.palette.background.paper,
+    },
+});
 
 class LanguageList extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {languages: []};
+        this.state = {
+            languages: [],
+            checked: [0]
+        };
     }
 
     componentWillMount() {
@@ -14,23 +33,60 @@ class LanguageList extends React.Component {
         })
     }
 
+    handleToggle = value => () => {
+        const { checked } = this.state;
+        const currentIndex = checked.indexOf(value);
+        const newChecked = [...checked];
+
+        if (currentIndex === -1) {
+            newChecked.push(value);
+        } else {
+            newChecked.splice(currentIndex, 1);
+        }
+
+        this.setState({
+            checked: newChecked,
+        });
+    };
+
+
     render() {
         let languages = this.state.languages;
+        const { classes } = this.props;
 
         return (
-            <div>
+            <Grid className={classes.root}>
+            <div className={classes.list}>
                 <h3>Language List</h3>
-                <ol>
-                {
-                    languages.map(lang => (
-                        <li key={lang.id}><Link to={`/languages/${lang.id}`}>{lang.desc}</Link></li>
-                    ))
-                }
-                </ol>
+                <List>
+                    {languages.map(lang => (
+                        <ListItem
+                            key={lang.id}
+                            dense
+                            button
+                            onClick={this.handleToggle(lang.id)}
+                            className={classes.listItem}
+                        >
+                            <Checkbox
+                                checked={this.state.checked.indexOf(lang.id) !== -1}
+                                tabIndex={-1}
+                                disableRipple
+                            />
+                            <li key={lang.id}><Link to={`/languages/${lang.id}`}>{lang.desc}</Link></li>
+                            {/*<ListItemText primary={`${lang.desc}`} />*/}
+                        </ListItem>
+                    ))}
+                </List>
                 <h5><Link to='/about'>About us</Link></h5>
             </div>
+            </Grid>
         );
     }
 }
 
-export default withAxios(LanguageList);
+LanguageList.propTypes = {
+    classes: PropTypes.object.isRequired,
+};
+
+
+export default withStyles(styles)(withAxios(LanguageList));
