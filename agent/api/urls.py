a1c5ea5b2d0477f95738a2c0097a5c80e4a25f45
Conke Hu <conke.hu@gmail.com>
2017-11-07 23:56:11 +0800

    add languages
    Signed-off-by: Conke Hu <conke.hu@gmail.com>

---
+++ agent/api/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import url
+
+from . import views
+
+urlpatterns = [
+    url(r'^$', views.index, name='index'),
+    url(r'^languages$', views.get_languages, name='get_languages'),
+]
--- agent/api/views.py
+++ agent/api/views.py
@@ -4,3 +4,20 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import HttpResponse, JsonResponse
+
+
+def index(request):
+    return render(request, 'index.html')
+
+def get_languages(request):
+    languages = [{
+          "id": "cxx",
+          "desc": "C/C++"
+        },
+        {
+          "id": "java",
+          "desc": "Java"
+        }]
+
+    return JsonResponse(languages, safe=False)
